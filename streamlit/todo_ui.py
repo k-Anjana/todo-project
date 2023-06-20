3de69b94f6244f6294c60ff045c9c0b90f01a7e5
JagadishR17K <jagadish.kollu@toucanus.com>
2023-06-20 23:13:29 +0530

    added profile pic

--- streamlit/todo_ui.py
+++ streamlit/todo_ui.py
@@ -115,44 +115,45 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
     token=st.session_state['token']
     username=st.session_state['username']
 
-    selected=option_menu(menu_title="Menu",
-        options=["To-Do","History"],
-        menu_icon="cast",
-        icons=["card-checklist","h-square"],
-        orientation="horizontal",)
+    col3,col4=st.columns([8,2])
 
-    if selected=="To-Do":
+    with col3:
+        selected=option_menu(menu_title="Menu",
+            options=["To-Do","History"],
+            menu_icon="cast",
+            icons=["card-checklist","h-square"],
+            orientation="horizontal",)
 
-        col1,col2=st.columns(2)
+        if selected=="To-Do":
 
-        with col1:
-            with st.form(key="adddtask_form",clear_on_submit=True):
-                task=st.text_input("Task")
-                add_button=st.form_submit_button("Add Task")
-            record_data={
-                "username":username,
-                "task":task,
-                "description":"",
-                "status":"pending",
-            }
-            if add_button:
-                
-                url=local_host+'todo/?type=insert'
-                headers = {'Authorization': f'Bearer {token}'}
-                response=requests.post(url,headers=headers,params=record_data)
-                if response.status_code==200:
-                    st.write("Task added successfully")
-                
-        with col2:
-            col1,col2=st.columns([1,2])
+            col1,col2=st.columns(2)
+
+            with col1:
+                with st.form(key="adddtask_form",clear_on_submit=True):
+                    task=st.text_input("Task")
+                    add_button=st.form_submit_button("Add Task")
+                record_data={
+                    "username":username,
+                    "task":task,
+                    "description":"",
+                    "status":"pending",
+                }
+                if add_button:
+                    
+                    url=local_host+'todo/?type=insert'
+                    headers = {'Authorization': f'Bearer {token}'}
+                    response=requests.post(url,headers=headers,params=record_data)
+                    if response.status_code==200:
+                        st.write("Task added successfully")
+                    
             with col2:
+
                 url=local_host+'todo/?type=fetch_pending'
                 headers = {'Authorization': f'Bearer {token}'}
                 params={
                     "username":username
                 }
                 response=requests.get(url,headers=headers,params=params)
-
                 if response.status_code==200:
                     record=response.json()
                     task=record['tasklist']
@@ -160,9 +161,7 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
                         task_button=st.checkbox(task[item],key=f'{item}{task[item]}')
                         if task_button:
                             update_button=st.radio("Complete or Delete",("Complete task","Delete task"),key=f'{item}')
-
                             if update_button=='Complete task':
-
                                 with st.container():
                                     with st.form(key=f"update_form.{item}",clear_on_submit=True):
                                         description = st.text_area("Description")
@@ -187,7 +186,6 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
                                                     st.write(update_message)
                                                     st.write(update_message['message'])
                                                     st.experimental_rerun()
-
                             elif update_button=='Delete task':
                                 
                                 url=local_host+'todo/?type=delete'
@@ -205,59 +203,64 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
                 else:
                     st.error("Unable to fetch the data")
 
-                
-    if selected=="History":
-        col1,col2=st.columns(2)
 
-        with col1:
-            url=local_host+'todo/?type=fetch_total'
-            headers = {'Authorization': f'Bearer {token}'}
-            params={
-                "username":username
-            }
-            response=requests.get(url,headers=headers,params=params)
-            if response.status_code==200:
-                task_history=response.json()
-                st.header("Total Tasks")
-                df = pd.DataFrame(task_history)
-                df.index = [i+1 for i in range(len(df))]
-                df.index.name = 'S.No'
-                st.dataframe(df, height=None)
+        if selected=="History":
+            col1,col2=st.columns(2)
 
-        with col2:
-            params={
-                "username":username,
-            }     
-        
-            url = local_host + "todo/?type=fetch_done"
-            headers = {'Authorization': f'Bearer {token}'}
-            response = requests.get(url,headers=headers,params=params)
+            with col1:
+                url=local_host+'todo/?type=fetch_total'
+                headers = {'Authorization': f'Bearer {token}'}
+                params={
+                    "username":username
+                }
+                response=requests.get(url,headers=headers,params=params)
+                if response.status_code==200:
+                    task_history=response.json()
+                    st.header("Total Tasks")
+                    df = pd.DataFrame(task_history)
+                    df.index = [i+1 for i in range(len(df))]
+                    df.index.name = 'S.No'
+                    st.dataframe(df, height=None)
+
+            with col2:
+                params={
+                    "username":username,
+                }     
             
-            if response.status_code == 200:
-                record = response.json()
-                tasks = record['tasks']
-                files = record['files']
-                description = record['description']
-                st.header("Completed Tasks")
-                for i in range(len(tasks)):
-                    done_task = st.button(f'{i+1}.{tasks[i]}')
-                    button_style = """
-                        <style>
-                        .stButton>button {
-                            background: none;
-                            border: none;
-                            padding: 0;
-                            margin: 0;
-                            font-size: inherit;
-                            font-family: inherit;
-                            cursor: pointer;
-                            outline: inherit;
-                        }
-                        </style>
-                    """
-                    st.markdown(button_style, unsafe_allow_html=True)
-                    if done_task:
-                        st.write("Description:", description[i])                        
-                        st.write("Click the link to download the file:", files[i])         
-            else:
-                st.error("Unable to fetch the data")       
+                url = local_host + "todo/?type=fetch_done"
+                headers = {'Authorization': f'Bearer {token}'}
+                response = requests.get(url,headers=headers,params=params)
+                
+                if response.status_code == 200:
+                    record = response.json()
+                    tasks = record['tasks']
+                    files = record['files']
+                    description = record['description']
+                    st.header("Completed Tasks")
+                    for i in range(len(tasks)):
+                        done_task = st.button(f'{i+1}.{tasks[i]}')
+                        button_style = """
+                            <style>
+                            .stButton>button {
+                                background: none;
+                                border: none;
+                                padding: 0;
+                                margin: 0;
+                                font-size: inherit;
+                                font-family: inherit;
+                                cursor: pointer;
+                                outline: inherit;
+                            }
+                            </style>
+                        """
+                        st.markdown(button_style, unsafe_allow_html=True)
+                        if done_task:
+                            st.write("Description:", description[i])                        
+                            st.write("Click the link to download the file:", files[i])         
+                else:
+                    st.error("Unable to fetch the data")  
+
+    with col4:
+        profile_photo="/home/jagadish/Downloads/profile-pic1.png"
+        st.image(profile_photo,width=350)
+        st.markdown(f"<h1 style='text-align: center; '>{username}</h1> <br>", unsafe_allow_html=True)
